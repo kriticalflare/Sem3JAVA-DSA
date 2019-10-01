b9241803a33a271fbc899f0ff6dfe6a4f63eb70f
kriticalflare <42350771+kriticalflare@users.noreply.github.com>
2019-10-01 21:53:24 +0530

    Initial java programs

---
+++ JAVA/commandline arguments/Add.java
@@ -0,0 +1,10 @@
+class Add{
+ public static void main(String args[]){
+ int x,y;
+ x = Integer.parseInt(args[0]);
+     y = Integer.parseInt(args[1]);
+int sum;
+     sum = x + y;
+     System.out.println(sum);
+ }
+}
